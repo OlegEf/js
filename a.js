f44299aecd7673269d73f8a4f504b4320d6be4c0
oiu7 <5tg6yy@gmail.com>
2020-01-22 22:12:41 +0200

    2 comit 2020.1.22 22:12

---
+++ a.js
@@ -0,0 +1,14 @@
+mosheinica - Ekaterina Hernik
+tai atoksil, belii ugol
+nifurarcfpbl
+enterol
+smekta sorbex
+
+
+var a=1;
+var b="222SSfgVVVVOOOOOOOOOOOvariableZZZZ"+"<br>";
+document.write(a);
+document.write(b);
+document.write(b);
+document.write(b);
+document.write(b);
--- index.js
+++ index.js
@@ -1,4 +1,104 @@
+var q1="<hr class=\"q1\">";
+var q2="<hr class=\"q2\">";
+var q3="<hr class=\"q3\">";
+var q4="<hr class=\"q4\">";
+var q5="<hr class=\"q5\">";
+var q6="<hr class=\"q6\">";
+var q7="<hr class=\"q7\">";
+var q8="<hr class=\"q8\">";
+var q9="<hr class=\"q9\">";
+var q10="<hr class=\"q10\">";
+var q11="<hr class=\"q11\">";
+var q12="<hr class=\"q12\">";
 var a=12;
+var a=12;
+// var br
+var b="<br>";
 var b="red"+"<br>";
-document.write(a);
-document.write(b);
+document.write(q1);
+document.write(a,b);
+document.write(a,b);
+document.write(a,b);
+// document.write(a);
+// document.write(b);
+// document.write(a);
+// fcc 143 qq
+var b="<br>";
+var you='You\'re right, it can\'t be \n a quote ';
+// var asd=ee+'You\'re right, it can\'t be \n a quote asdfdfgf ';
+
+document.write(you,b);
+
+function convertToF(celsius){
+	var fahrneheit=(celsius*(9/5))+32;
+	return fahrneheit;
+}
+convertToF(30);
+// asd=convertToF(celsius);
+// asd=fahtrnheit;
+asd=convertToF(30);
+// asd=convertToF;
+document.write(asd);
+document.write(q2,q3,q4,b);
+// fcc 142 ++
+var myFirstName = "Alan";
+var myLastName = "Turing";
+// experement
+var myLastName = "Turing is bes\'t pan\n da \u03c0 guravel\r asd";
+document.write(myFirstName);
+document.write(myLastName);
+document.write(q6,q3,q4,b);
+//qq tav
+//qq
+document.write(q6,q3,q4,b);
+//fcc 144
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+document.write(myStr,b);
+
+//fcc 14
+var myStr='FirstLine\n\\SecondLine\\\rThirdLine'; // Change this line
+var myStrBr='FirstLine'+b+'SecondLine'+b+'ThirdLine'; // Change this line
+document.write(myStr,b);
+document.write(myStrBr);
+document.write(q1,q9,b);
+
+//fcc 147 2020.1.22 22:10
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr= "This is the first sentence. ";
+myStr += "This is the second sentence.";
+document.write(myStr,b,ourStr);
+
+//fcc 14
+document.write(q1,q9,b);
+
+//fcc 14
+document.write(q1,q9,b);
+
+//fcc 14
+document.write(q1,q9,b);
+
+//fcc 14
+document.write(q1,q9,b);
+
+//fcc 14
+document.write(q1,q9,b);
+
+//fcc 14
+document.write(q1,q9,b);
+
+//fcc 14
+document.write(q1,q9,b);
+
+//fcc 14
+document.write(q1,q9,b);
+
+//fcc 14
+document.write(q1,q9,b);
+
+//fcc 14
+document.write(q1,q9,b);
